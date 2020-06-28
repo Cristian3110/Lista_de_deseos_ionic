cf33b2556aa8956c2575037368b0ce548b0cff79
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-06-28 02:38:02 -0400

    configuración del components listas, ts y html

--- src/app/components/listas/listas.component.ts
+++ src/app/components/listas/listas.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DeseosService } from "../../services/deseos.service";
+import { Lista } from "../../models/lista.model";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-listas',
-  templateUrl: './listas.component.html',
-  styleUrls: ['./listas.component.scss'],
+  selector: "app-listas",
+  templateUrl: "./listas.component.html",
+  styleUrls: ["./listas.component.scss"],
 })
 export class ListasComponent implements OnInit {
-
-  constructor() { }
+  constructor(public deseosService: DeseosService, private router: Router) {}
 
   ngOnInit() {}
 
+  listaSeleccionada(lista: Lista) {
+    console.log(lista);
+    this.router.navigateByUrl(`/tabs/agregar/${lista.id}`);
+  }
 }
